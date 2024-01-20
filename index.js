2641dc3d94b6b85ac19ca2e07f5aa3cd52848fe6
samandargofurov <samandargofurov236@gmail.com>
2024-01-20 23:13:09 +0500

    5 lesson

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // 0. 1 dan 100 gacha bo'lgan sonlarni ekranga chiqaruvchi algoritm yozing
 
-
+// let a = 100;
+// for (let i = 1; i <= 100; i++){
+//     console.log(i);
+// }
 
 // 1. 1 dan n gacha bo'lgan sonlarni ekranga chiqaruvchi dastur yozing
 
