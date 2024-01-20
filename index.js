26f957d4c629b989a9b112f9c65455656968d98e
samandargofurov <samandargofurov236@gmail.com>
2024-01-20 22:38:42 +0500

    5 lesson

---
+++ index.js
@@ -0,0 +1,63 @@
+// 0. 1 dan 100 gacha bo'lgan sonlarni ekranga chiqaruvchi algoritm yozing
+
+
+
+// 1. 1 dan n gacha bo'lgan sonlarni ekranga chiqaruvchi dastur yozing
+
+// let a = 50;
+// for (let i = 1; i <= 100; i++){
+//     console.log(i);
+// }
+
+// 2. 1 dan n gacha sonlar yigindisini hisoblovchi algoritm yozing
+
+// let a = 9;
+// let sum = 0;
+// for (let i = 1; i <= a; i++) {
+//     sum += i;
+// }
+
+// console.log(sum);
+
+// 3. 1 dan n gacha barcha toq sonlar yigindisini topuvchi algoritm yozing
+
+// let oddNumber = 10;
+// let sum = 0;
+// for (let i = 1; i <= oddNumber; i++) {
+//     if (i % 2 == 1) {
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+
+// 4. a dan b gacha barcha juft sonlar kopaytmasini topuvchi algoritm yozing
+
+// let a = 1;
+// let b = 9;
+// let sum = 1;
+// for (let i = a; i <= b; i++) {
+//     if (i % 2 == 0) {
+//         sum *= i;
+//     }
+// }
+// console.log(sum);
+
+// 5. a dan b gacha barcha 3 ga hamda 5 ga karrali sonlar yigindisini topuvchi algoritm yozign
+
+// let a = 15;
+// let b = 35;
+// let sum = 0;
+// for (let i = a; i <= b; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         sum += i;
+//     }
+// }
+
+// console.log(sum);
+
+// 6. Berilgan sonning bo'luvchilar sonini topuvchi algoritm yozing
+
+
+
+// 7. Berilgan sonning tub yoki tub emasligini aniqlovchi algoritm yozing
+
